981ecbde9df14919a94c89fd34325df4923d2ccb
RahulIyerK <rahul.iyer2@gmail.com>
2015-10-24 11:45:43 -0700

    changed analyzeSpeech

--- analyzeSpeech.php
+++ analyzeSpeech.php
@@ -17,8 +17,8 @@
 // $result= curl_exec ($ch);
 // echo ($result);
 // curl_close ($ch);
-$cmd = 'curl -u 5fca9714-65c7-4789-9c1c-fc4345ec9b66:0vX8TffEgqTV -X POST --limit-rate 40000 --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @0001.flac "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"';
-// exec($cmd);
-$obj = json_decode(exec($cmd));
-echo($obj[0]->text);
+//$cmd = 'curl -u 5fca9714-65c7-4789-9c1c-fc4345ec9b66:0vX8TffEgqTV -X POST --limit-rate 40000 --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @0001.flac "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"';
+$cmd = 'ping google.com';
+exec($cmd);
+
 ?>
