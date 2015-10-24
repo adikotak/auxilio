227d704ddbd49f7e7416dd3a5b98643dc3db0e0d
RahulIyerK <rahul.iyer2@gmail.com>
2015-10-24 01:38:35 -0700

    updated

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
   <?php include ('nav.php'); ?>
-  <body>
 
-    <h1>Hello, world!</h1>
+<body>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+  <h1>Auxilio</h1>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+
+  <button class="btn btn-default" href = "email.php">
+    Email
+  </button>
+
+  <button class="btn btn-default" href = "essay.php">
+    Essay
+  </button>
+
+
+</body>
 </html>
