90a2121d0d3ecd0a302b496ab6e75167204b6ffb
RahulIyerK <rahul.iyer2@gmail.com>
2015-10-24 03:45:52 -0700

    added slang analysis --RI

--- analyzeEmail.php
+++ analyzeEmail.php
@@ -3,8 +3,38 @@
   <?php include ('nav.php'); ?>
   <body>
     <div class ="container">
+
 <?php
-   echo( $_POST['emailEntry'] . "<br />\n");
+  $originalEntry = $_POST['emailEntry'];
+  $splitRegex = "/[\s]/";
+  $words = preg_split($splitRegex, $originalEntry);
+
+  $numWords = count($words);
+
+  $db = "ad_cda192a887ff69f";
+  $con = new mysqli("us-cdbr-iron-east-03.cleardb.net", "be69c4201781ac", "abaf0684", $db);
+  $table = "slangwords";
+
+  for ($i=0; $i<$numWords; $i++)
+  {
+
+    $query = "SELECT * FROM $table";
+    $result = $con->query($query);
+    if ($result->num_rows>0)
+    {
+      while ($row = $result->fetch_assoc())
+      {
+          $word = $row['words'];
+          if ($words[$i] == $word)
+          {
+            echo($words[$i]);
+          }
+		  }
+		}
+
+  }
+  mysqli_close($con);
+
 ?>
 </div>
 </body>
