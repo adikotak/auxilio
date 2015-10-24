01ea9056d0873054780c20ea6232a4820175c49f
srinjoym <srinjoy.majumdar@gmail.com>
2015-10-24 04:12:44 -0400

    added nav bar file

---
+++ email.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include ('nav.php'); ?>
+  <body>
+    <div class = "container">
+      <div class = "form-group">
+    <form method ="POST" action ="analyzeEmail.php">
+        <textarea class = "form-control text" name = "emailEntry" >
+           Write something here
+        </textarea>
+    </form>
+  </div>
+  </div>
+  </body>
+  </html>
