9eac79c10dfdf39dfb40800b54543803424ff920
adikotak <aditya@Adityas-MacBook-Pro.local>
2015-10-25 04:08:08 -0400

    finall index

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 			<div class="text" style="padding-top:50px; margin-left:350px; width:800px">
 			<p style="font-family:'Open Sans'; font-weight:300; color:#fff; margin-top:60px; margin-left:120px; font-size:35px;">Writing. Better.</p>
 			<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:30px; text-align:left; margin-left:120px; font-size:22px;">Get contextual feedback from Auxilio's assistive writing feature to never have another typo. Using the Automattic grammar API, grammar mistakes and spelling errors are a thing of the past thanks to Auxilio.</p>
+			<span class="glyphicon glyphicon-pencil" aria-hidden="true" style = "position: absolute; top: 140%; left: 15%; margin-right: -50%; transform: translate(-50%, -50%)">  </span>
 			<center>
 			<a href="essay.php" class= "btn btn-default btn-md" style= "font-family:'Open Sans'; font-size:15px; vertical-align: middle; margin-right: 5vh";>Check Your Essay!</a>
 		</center>
@@ -31,6 +32,7 @@
 			<div class="text2" style="padding-top:50px; margin-left:350px; width:800px">
 				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; margin-top:60px; margin-left:120px; font-size:35px;">Writing. Reimagined.</p>
 				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:30px; text-align:left; margin-left:120px; font-size:22px;">Learn to improve writing through Auxilio's smart suggestive features. Auxilio's smart engine automatically makes suggestions for your emails to make them more formal and precise. Auxilio will offer suggestions from adding details for meetings to making changes from colloquial terms.</p>
+				<span class="glyphicon glyphicon-sunglasses" aria-hidden="true" style = "position: absolute; top: 240%; left: 15%; margin-right: -50%; transform: translate(-50%, -50%)">  </span>
 				<center>
 				<a href="email.php" class= "btn btn-default btn-md" style= "font-family:'Open Sans'; font-size:15px; vertical-align: middle; margin-top: 3vh; margin-right: 5vh";>Check Your Email!</a>
 			</center>
@@ -42,6 +44,8 @@
 			<div class="text" style="padding-top:50px; margin-left:350px; width:800px">
 			<p style="font-family:'Open Sans'; font-weight:300; color:#fff; margin-top:60px; margin-left:120px; font-size:35px;">Writing. Smarter.</p>
 			<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:30px; text-align:left; margin-left:120px; font-size:22px;">With the use of IBM BlueMIX's Watson Speech to Text API, users can dictate their writings into Auxilio for a new dimension of writing profreereading.</p>
+			<span class="glyphicon glyphicon-education" aria-hidden="true" style = "position: absolute; top: 330%; left: 15%; margin-right: -50%; transform: translate(-50%, -50%)">  </span>
+
 			<center>
 			<a href="getfile.php" class= "btn btn-default btn-md" style= "font-family:'Open Sans'; font-size:15px; vertical-align: middle; margin-right: 5vh";>Check Your Speech!</a>
 		</center>
@@ -52,20 +56,28 @@
 		<div class="team" style="height:95vh; background-color:#336E7B; margin-top:0px">
 			<div class="teamText" style="padding-top:50px; margin-left:350px; width:800px">
 				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; margin-top:60px; margin-left:120px; font-size:35px;">Team.</p>
-				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:30px; text-align:left; margin-left:120px; font-size:22px;">We are a team of high school students who created Auxilio at Hacking EDU.</p>
+				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:20px; text-align:left; margin-left:120px; font-size:22px;">We are a team of high school students who created Auxilio at Hacking EDU.</p>
+				<span class="glyphicon glyphicon-user" aria-hidden="true" style = "position: absolute; top: 420%; left: 15%; margin-right: -50%; transform: translate(-50%, -50%)">  </span>
+				<img src="joyboy.jpg" alt="srinjoy" style="width:200px;height:200px; position: absolute; top: 440%; left: 10%;">
+				<p style="fond-family: 'Open Sans' font-weight:300; color:#fff; position: absolute; top: 470%; left: 10%; font-size:20px;">Srinjoy Majumdar, 12</p>
+				<img src="rahul.jpg" alt="rahul" style="width:200px;height:200px; position: absolute; top: 440%; left: 40%;">
+				<p style="fond-family: 'Open Sans' font-weight:300; color:#fff; position: absolute; top: 470%; left: 42.5%; font-size:20px;">Rahul Iyer, 12</p>
+				<img src="kotak.jpg" alt="aditya" style="width:200px;height:200px;position: absolute; top: 440%; left: 70%;">
+				<p style="fond-family: 'Open Sans' font-weight:300; color:#fff; position: absolute; top: 470%; left: 72.5%; font-size:20px;">Aditya Kotak, 12</p>
 			</div>
 		</div>
 		<section id ="contactAbout"></a>
 		<div class="contactAbout" style="height:95vh; background-color:#03C9A9; margin-top:0px">
 			<div class="contactAbout" style="padding-top:50px; margin-left:350px; width:800px">
 				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; margin-top:60px; margin-left:120px; font-size:35px;">Contact Us.</p>
-				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:30px; text-align:left; margin-left:120px; font-size:22px;">If you're interested in Auxilio feel free to email any of us.</p>
+				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:30px; text-align:left; margin-left:120px; font-size:22px;">If you're interested in Auxilio, feel free to email any of us.</p>
 				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:30px; text-align:left; margin-left:120px; font-size:22px;"></p>
 				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:10px; text-align:left; margin-left:120px; font-size:22px;">Srinjoy Majumdar: srinjoy.majumdar@gmail.com.</p>
 				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:10px; text-align:left; margin-left:120px; font-size:22px;">Rahul Iyer: rahul.iyer2@gmail.com.</p>
 				<p style="font-family:'Open Sans'; font-weight:300; color:#fff; padding-top:10px; text-align:left; margin-left:120px; font-size:22px;">Aditya Kotak: adikotak@gmail.com.</p>
+				<span class="glyphicon glyphicon-envelope" aria-hidden="true" style = "position: absolute; top: 520%; left: 15%; margin-right: -50%; transform: translate(-50%, -50%)">  </span>
 			</div>
 		</div>
 	</body>
-	
+
 </html>
