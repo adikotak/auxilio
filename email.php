0e97203cbccfd238fbcd9e65ae5eab328c3d9908
adikotak <aditya@Adityas-MacBook-Pro.local>
2015-10-24 21:00:15 -0400

    updated navbar

--- email.php
+++ email.php
@@ -2,7 +2,8 @@
 <html lang="en">
   <?php include ('nav.php'); ?>
   <body>
-    <div class = "container">
+    <div class = "rip" style = "background-color: #59ABE3" </div>
+    <div class = "container" style = "background-color: #59ABE3">
       <div class = "form-group">
     <form method ="post" action ="analyzeEmail.php">
         <textarea class = "form-control text" name = "emailEntry" >
--- nav.php
+++ nav.php
@@ -28,47 +28,40 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="#">Brand</a>
+    <a class="navbar-brand" href="#home">Home</a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li role="separator" class="divider"></li>
-          <li><a href="#">Separated link</a></li>
-          <li role="separator" class="divider"></li>
-          <li><a href="#">One more separated link</a></li>
+
         </ul>
       </li>
     </ul>
-    <form class="navbar-form navbar-left" role="search">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search">
-      </div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li role="separator" class="divider"></li>
-          <li><a href="#">Separated link</a></li>
+
+    <li role="separator" class="divider"></li>
+          <li><a href="#featureEssay">Auxilio for Essays</a></li>
+          <li><a href="#featureEmail">Auxilio for Emails</a></li>
+          <li><a href="#team">Team</a></li>
+          <li><a href="#contactAbout">Contact</a></li>
         </ul>
       </li>
     </ul>
   </div><!-- /.navbar-collapse -->
 </div><!-- /.container-fluid -->
 </nav>
+
+<script src="js/smooth-scroll.min.js"></script>
+<script>
+smoothScroll.init({
+        speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeOutQuad', // Easing pattern to use
+    updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+    offset: 79, // Integer. How far to offset the scrolling anchor location in pixels
+    callbackBefore: function ( toggle, anchor ) {}, // Function to run before scrolling
+    callbackAfter: function ( toggle, anchor ) {} // Function to run after scrolling
+  });
+</script>
+
 </head>
