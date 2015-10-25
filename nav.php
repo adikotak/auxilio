4ecf6d2495694366c61325c88c75024f577aa354
srinjoym <srinjoy.majumdar@gmail.com>
2015-10-24 23:15:55 -0400

    added smooth scrolling again

--- nav.php
+++ nav.php
@@ -18,8 +18,9 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
+  <div class="container-fluid">
   <nav class="navbar navbar-default navbar-fixed-top" style = "background-color:#FFFFFF">
-<div class="container-fluid">
+
   <!-- Brand and toggle get grouped for better mobile display -->
   <div class="navbar-header">
     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -28,7 +29,7 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="#home" >Home</a>
+    <a class="navbar-brand" data-scroll href="index.php#home" >Home</a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
@@ -41,24 +42,25 @@
     <ul class="nav navbar-nav navbar-right">
 
     <li role="separator" class="divider"></li>
-          <li><a href="#featureEssay">Auxilio for Essays</a></li>
-          <li><a href="#featureEmail">Auxilio for Emails</a></li>
-          <li><a href="#team">Team</a></li>
-          <li><a href="#contactAbout">Contact</a></li>
+          <li><a data-scroll href="#featureEssay">Auxilio for Essays</a></li>
+          <li><a data-scroll href="#featureEmail">Auxilio for Emails</a></li>
+          <li><a data-scroll href="#team">Team</a></li>
+          <li><a data-scroll href="#contactAbout">Contact</a></li>
         </ul>
       </li>
     </ul>
   </div><!-- /.navbar-collapse -->
+  </nav>
 </div><!-- /.container-fluid -->
-</nav>
+
 
 <script src="js/smooth-scroll.min.js"></script>
 <script>
 smoothScroll.init({
-        speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+        speed: 750, // Integer. How fast to complete the scroll in milliseconds
     easing: 'easeOutQuad', // Easing pattern to use
     updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
-    offset: 79, // Integer. How far to offset the scrolling anchor location in pixels
+    offset: 35, // Integer. How far to offset the scrolling anchor location in pixels
     callbackBefore: function ( toggle, anchor ) {}, // Function to run before scrolling
     callbackAfter: function ( toggle, anchor ) {} // Function to run after scrolling
   });
