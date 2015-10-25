f5f1cabc0ff08327e8255e4a0564ad04dadec11a
srinjoym <srinjoy.majumdar@gmail.com>
2015-10-24 20:08:21 -0400

    back to where we were 4hrs ago

---
+++ js/custom.js
@@ -0,0 +1,1 @@
+body onload="initForm(document.forms[0], 'essayEntry', 'Please enter your complaint in brief')";
--- nav.php
+++ nav.php
@@ -18,7 +18,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <nav class="navbar navbar-default navbar-fixed-top" style = "background-color: #FFFFFF">
+  <nav class="navbar navbar-default navbar-fixed-top">
 <div class="container-fluid">
   <!-- Brand and toggle get grouped for better mobile display -->
   <div class="navbar-header">
@@ -28,19 +28,43 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class= logo> <img src = "aLogo.png" style = "margin-top: 10px" href="#home"></a>
+    <a class="navbar-brand" href="#">Brand</a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <ul class="nav navbar-nav"></ul>
-    <form class="navbar-form navbar-left"
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+      <li><a href="#">Link</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li role="separator" class="divider"></li>
+          <li><a href="#">Separated link</a></li>
+          <li role="separator" class="divider"></li>
+          <li><a href="#">One more separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+    <form class="navbar-form navbar-left" role="search">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
     </form>
     <ul class="nav navbar-nav navbar-right">
-          <li><a class="waves-effect waves" data-scroll href="#featureEssay">Auxilio for Essay</a></li>
-          <li><a class="waves-effect waves" data-scroll href="#featureEmail">Auxilio for Email</a></li>
-          <li><a class="waves-effect waves" data-scroll href="#team">Team</a></li>
-          <li><a class="waves-effect waves" data-scroll href="#contactAbout">Contact Us</a></li>
+      <li><a href="#">Link</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li role="separator" class="divider"></li>
+          <li><a href="#">Separated link</a></li>
         </ul>
       </li>
     </ul>
